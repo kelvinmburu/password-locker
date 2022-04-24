90f0f30e3148f27a76df8d8443e6205dee3dbfca
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-24 21:00:13 +0300

    refactor: added function and method to display user list

--- locker.py
+++ locker.py
@@ -3,3 +3,20 @@ import string
 import pyperclip
 
 # Creating class to generate new instance of an user
+
+
+class User:
+
+    user_list = []
+
+    # Function to highlight properties of a user_list
+
+    def save_user(self):
+
+        User.self_list.append(self)
+
+        # Method to display user list
+
+    @classmethod
+    def display_user_list(cls):
+        return cls.user_list
