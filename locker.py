a339991a8061c2770971b0531c6c2a1fc7de556e
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-24 21:06:13 +0300

    refactor: added loop to check user verification

--- locker.py
+++ locker.py
@@ -20,3 +20,26 @@ class User:
     @classmethod
     def display_user_list(cls):
         return cls.user_list
+
+    # Create function to delete user user_list
+
+    def delete_user(self):
+
+        User.self_list.remove(self)
+
+# New class to create a new instance of credentials
+
+
+class Credentials():
+    credentials_list = []
+
+    # Add function to verify user credentials
+    @classmethod
+    def verify_user(cls, username, password):
+
+        a_user = ""
+
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                a_user = user.username
+                return a_user
